282212127e669541a9c42d51bbd69db62defb900
Ward <50712170+Ward727a@users.noreply.github.com>
2022-12-26 18:31:59 +0100

    update app

--- src/renderer/components/barter/BarterCenterItem.tsx
+++ src/renderer/components/barter/BarterCenterItem.tsx
@@ -8,12 +8,16 @@ type Props = {
     data: dataDict;
     index: number;
     setValTotal: (qty: number, tier: number) => void;
+    hideBool: (hide: boolean, type: string) => void;
     hide?: boolean;
+    hideIliya?: boolean;
+    hideEpheria?: boolean;
+    hideAncado?: boolean;
 }
 
 const win:win_ = window;
 
-// TODO: Add the threshold warning for each city
+// // TODO: Add the threshold warning for each city
 // // TODO: Add the tier color system for each item
 // // TODO: Add the sort by tier system for each item
 
@@ -22,16 +26,25 @@ const BarterCenterItem: React.FC<Props> = (props: Props) => {
     const key = Object.keys(props.data.item)[props.index];
     const index = props.index;
 
+    const hideAncado = props.hideAncado
+    const hideEpheria = props.hideEpheria
+    const hideIliya = props.hideIliya
+
     const className="tier-"+props.data.item[key][0].tier[0]
 
+    const save = props.data.save.items[0][key]? props.data.save.items[0] : {[key]:[{qty: ["0"], iliya: ["0"], epheria: ["0"], ancado: ["0"]}]};
+
+    if(!props.data.save.items[0][key]) {
+        props.data.save.items[0][key] = [{qty: ["0"], iliya: ["0"], epheria: ["0"], ancado: ["0"]}]
+    }
+
     // Create state for each city
-    const [iliya, _setIliya] = React.useState(parseInt(props.data.save.items[0][key][0].iliya[0]));
-    const [epheria, _setEpheria] = React.useState(parseInt(props.data.save.items[0][key][0].epheria[0]));
-    const [ancado, _setAncado] = React.useState(parseInt(props.data.save.items[0][key][0].ancado[0]));
+    const [iliya, _setIliya] = React.useState(parseInt(save[key][0].iliya[0]? save[key][0].iliya[0] : "0"));
+    const [epheria, _setEpheria] = React.useState(parseInt(save[key][0].epheria[0]? save[key][0].epheria[0] : "0"));
+    const [ancado, _setAncado] = React.useState(parseInt(save[key][0].ancado[0]? save[key][0].ancado[0] : "0"));
 
     // Create state for quantity combining all cities together
     const [quantity, setQuantity] = React.useState(iliya+epheria+ancado);
-
     
 
     // Create state for showing input element for each city
@@ -39,67 +52,102 @@ const BarterCenterItem: React.FC<Props> = (props: Props) => {
     const [showInputEleEpheria, setShowInputEleEpheria] = React.useState(false);
     const [showInputEleAncado, setShowInputEleAncado] = React.useState(false);
 
-    const [countIliya, setCountIliya] = React.useState(true);
-    const [countEpheria, setCountEpheria] = React.useState(true);
-    const [countAncado, setCountAncado] = React.useState(true);
+    const [countIliya, setCountIliya] = React.useState<boolean>(hideIliya);
+    const [countEpheria, setCountEpheria] = React.useState<boolean>(hideEpheria);
+    const [countAncado, setCountAncado] = React.useState<boolean>(hideAncado);
 
     const [limitIliya, setLimitIliya] = React.useState(parseInt(props.data.save.threshold[0].iliya[0]));
     const [limitEpheria, setLimitEpheria] = React.useState(parseInt(props.data.save.threshold[0].epheria[0]));
     const [limitAncado, setLimitAncado] = React.useState(parseInt(props.data.save.threshold[0].ancado[0]));
 
     barterEventManager.onThresholdChange('BarterCenterItem', (type, threshold) => {
-        switch(type){
-            case "iliya":
-                setLimitIliya(threshold)
-                break;
-            case "epheria":
-                setLimitEpheria(threshold)
-                break;
-            case "ancado":
-                setLimitAncado(threshold)
-                break;
-        }
+        new Promise((resolve)=>{
+            switch(type){
+                case "iliya":
+                    setLimitIliya(threshold)
+                    resolve(true)
+                    break;
+                case "epheria":
+                    setLimitEpheria(threshold)
+                    resolve(true)
+                    break;
+                case "ancado":
+                    setLimitAncado(threshold)
+                    resolve(true)
+                    break;
+                default:
+                    resolve(false)
+                    break;
+            }
+        })
     })
 
     
     barterEventManager.onHideColBarter('BarterCenterItem', (hide, type) => {
-        if(type === "iliya"){
-            setCountIliya(hide)
-        }else if(type === "epheria"){
-            setCountEpheria(hide)
-        }else if(type === "ancado"){
-            setCountAncado(hide)
-        }
 
-        countQuantity();
+        new Promise((resolve)=>{
+            props.hideBool(hide, type)
+            switch(type){
+                case "iliya":
+                    setCountIliya(hide)
+                    resolve(true)
+                    break;
+                case "epheria":
+                    setCountEpheria(hide)
+                    resolve(true)
+                    break;
+                case "ancado":
+                    setCountAncado(hide)
+                    resolve(true)
+                    break;
+                default:
+                    resolve(false)
+                    break;
+            }
+
+            setQuantity((countIliya?(isNaN(iliya) || iliya <=0)? 0:iliya :0)+(countEpheria?(isNaN(epheria) || epheria <=0)? 0:epheria :0)+(countAncado?(isNaN(ancado) || ancado <=0)? 0:ancado :0))
+
+        })
     })
 
 
     const countQuantity = () => {
 
-        if(isNaN(iliya) || iliya <= 0){
-            setIliya(0)
-        }
+        new Promise((resolve)=>{
+            
 
-        if(isNaN(epheria) || epheria <= 0){
-            setEpheria(0)
-        }
+            if(isNaN(iliya) || iliya <= 0){
+                setIliyaCenter(0)
+            }
 
-        if(isNaN(ancado) || ancado <= 0){
-            setAncado(0)
-        }
+            if(isNaN(epheria) || epheria <= 0){
+                setEpheriaCenter(0)
+            }
 
-        setQuantity((countIliya?(isNaN(iliya) || iliya <=0)? 0:iliya :0)+(countEpheria?(isNaN(epheria) || epheria <=0)? 0:epheria :0)+(countAncado?(isNaN(ancado) || ancado <=0)? 0:ancado :0))
+            if(isNaN(ancado) || ancado <= 0){
+                setAncadoCenter(0)
+            }
 
-        props.setValTotal(0, 1)
+            // setQuantity(iliya+epheria+ancado) 
+            // If a city is hidden, then don't count it in the total by setting it to 0
+            setQuantity((countIliya?(isNaN(iliya) || iliya <=0)? 0:iliya :0)+(countEpheria?(isNaN(epheria) || epheria <=0)? 0:epheria :0)+(countAncado?(isNaN(ancado) || ancado <=0)? 0:ancado :0))
+
+            props.setValTotal(0, 1)
+
+            resolve(true);
+        });
     }
 
 
-    const setIliya = (value: number)=>{
+    const setIliyaCenter = (value: number)=>{
         _setIliya(value)
 
         props.data.save.items[0][key][0].iliya[0] = value.toString();
 
+        // const data = props.data
+        // win.api.send('save-data-dict', {data: JSON.stringify(data)});
+
+        // save the value to the save file
         barterEventManager.saveItem('BarterCenterItem',
             key,
             value,
@@ -107,11 +155,15 @@ const BarterCenterItem: React.FC<Props> = (props: Props) => {
         )
     }
 
-    const setEpheria = (value: number)=>{
+    const setEpheriaCenter = (value: number)=>{
         _setEpheria(value)
 
         props.data.save.items[0][key][0].epheria[0] = value.toString();
 
+        // const data = props.data
+        // win.api.send('save-data-dict', {data: JSON.stringify(data)});
+
+        // save the value to the save file
         barterEventManager.saveItem('BarterCenterItem',
             key,
             value,
@@ -119,11 +171,15 @@ const BarterCenterItem: React.FC<Props> = (props: Props) => {
         )
     }
 
-    const setAncado = (value: number)=>{
+    const setAncadoCenter = (value: number)=>{
         _setAncado(value)
 
         props.data.save.items[0][key][0].ancado[0] = value.toString();
 
+        // const data = props.data
+        // win.api.send('save-data-dict', {data: JSON.stringify(data)});
+
+        // save the value to the save file
         barterEventManager.saveItem('BarterCenterItem', 
             key,
             value,
@@ -144,14 +200,14 @@ const BarterCenterItem: React.FC<Props> = (props: Props) => {
             <td>{props.data.item[key][0].tier[0]}</td>
             <td>{props.data.lang.items[0][key][0].name[0]}</td>
             <td>{quantity}</td>
-            <td className={`iliya-table-viewer ${iliya <= limitIliya ? 'warning-thresold' : ''}`}>
+            <td className={`iliya-table-viewer ${iliya <= limitIliya ? 'warning-thresold ' : ' '}` + (!countIliya ? 'hide-h' : '')}>
                 <ElementMaker
                     value={iliya}
                     handleChange={(e) =>{
                         if(isNaN(parseInt(e.target.value))){
-                            setIliya(0)
+                            setIliyaCenter(0)
                         }else{
-                        setIliya(parseInt(e.target.value))
+                        setIliyaCenter(parseInt(e.target.value))
                         }
                     }}
                     handleDoubleClick={() =>
@@ -166,12 +222,12 @@ const BarterCenterItem: React.FC<Props> = (props: Props) => {
                     limit={limitIliya}
                 />
             </td>
-            <td className={`epheria-table-viewer ${epheria <= limitEpheria ? 'warning-thresold' : ''}`}>
+            <td className={`epheria-table-viewer  ${!countEpheria ? 'hide-h' : ''} ${epheria <= limitEpheria ? 'warning-thresold' : ''}`}>
                 
                 <ElementMaker
                     value={epheria}
                     handleChange={(e) =>
-                        setEpheria(parseInt(e.target.value))
+                        setEpheriaCenter(parseInt(e.target.value))
                     }
                     handleDoubleClick={() =>
                         setShowInputEleEpheria(true)
@@ -185,11 +241,11 @@ const BarterCenterItem: React.FC<Props> = (props: Props) => {
                     limit={limitEpheria}
                 />
             </td>
-            <td className={`ancado-table-viewer ${ancado <= limitAncado ? 'warning-thresold' : ''}`}> 
+            <td className={`ancado-table-viewer  ${!countAncado ? 'hide-h' : ''} ${ancado <= limitAncado ? 'warning-thresold' : ''}`}>
                 <ElementMaker
                     value={ancado}
                     handleChange={(e) =>
-                        setAncado(parseInt(e.target.value))
+                        setAncadoCenter(parseInt(e.target.value))
                     }
                     handleDoubleClick={() =>
                         setShowInputEleAncado(true)
